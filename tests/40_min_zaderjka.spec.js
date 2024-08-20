704a8ceaefb1257508e83924a1ca67737a5a13a6
gamballo <dlyhbka@gmail.com>
2024-08-20 21:32:02 +0500

    wb-monik-poisk

---
+++ tests/40_min_zaderjka.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test');
+test('40 min zaderjka', async ({page}) => {
+    await page.goto('https://www.wildberries.ru/');
+    await page.waitForTimeout(1000);
+    await page.locator('#searchInput').fill("Монитор")
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Enter')
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', {name: 'Все фильтры'}).click()
+    await page.waitForTimeout(1000);
+    await page.getByText('2 дня').first().click()
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'Показать', exact: true }).click()
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', {name: 'По популярности'}).click()
+    await page.waitForTimeout(1000);
+    await page.getByText('По возрастанию цены').first().click()
+    await page.waitForTimeout(1000);
+    await page.locator(".product-card__wrapper").nth(4).click()
+    await page.waitForTimeout(1000);
+
+});
--- tests/dnssearch.spec.js
+++ tests/dnssearch.spec.js
@@ -1,36 +1,23 @@
-const { test, expect } = require('@playwright/test');
-test('has footer', async ({page}) => {
-    await page.goto('https://www.wildberries.ru/');
-    await page.waitForTimeout(1000);
-    const navBurger = page.locator(".nav-element__burger")
-    await page.waitForTimeout(1000);
-    await navBurger.click()
-    await page.waitForTimeout(1000);
-     await page.locator(".menu-burger__main-list-link.menu-burger__main-list-link--4830").hover()
-     await page.waitForTimeout(1000);
-     await page.getByText("Ноутбуки и компьютеры").click()
-     await page.waitForTimeout(1000);
-     await page.getByText("Комплектующие для ПК").click()
-    await page.waitForTimeout(1000);
-    await page.getByText("Все фильтры").click()
-    await page.locator('.filter__item').filter({hasText: 'Видеокарта'}).click()
-    await page.getByText("NVIDIA GeForce RTX 4070").click()
-    await page.locator(".filters-desktop__btn-main.btn-main:visible").click()
-    //await page.getByRole('button', { name: 'Показать', exact: true }).click()
-    await page.waitForTimeout(3000);
-
-
-
-
-
-//     await locator1.click()
-//     await page.waitForTimeout(2000)
-//     const locator3 = page.locator(".tabs__list__arrow__next")
-//     await locator3.click()
-//     await page.waitForTimeout(2000)
-//     const locator4 = page.getByRole('link', { name: "КУБ-Бизнес" })
-//     await locator4.click()
-//     await page.waitForTimeout(2000)
-    //await page.locator(".menu-burger__main-list-link menu-burger__main-list-link--4830").click()
-    await page.waitForTimeout(1000);
-});
+// const { test, expect } = require('@playwright/test');
+// test('has footer', async ({page}) => {
+//     await page.goto('https://www.wildberries.ru/');
+//     await page.waitForTimeout(1000);
+//     const navBurger = page.locator(".nav-element__burger")
+//     await page.waitForTimeout(1000);
+//     await navBurger.click()
+//     await page.waitForTimeout(1000);
+//      await page.locator(".menu-burger__main-list-link.menu-burger__main-list-link--4830").hover()
+//      await page.waitForTimeout(1000);
+//      await page.getByText("Ноутбуки и компьютеры").click()
+//      await page.waitForTimeout(1000);
+//      await page.getByText("Комплектующие для ПК").click()
+//     await page.waitForTimeout(1000);
+//     await page.getByText("Все фильтры").click()
+//     await page.locator('.filter__item').filter({hasText: 'Видеокарта'}).click()
+//     await page.getByText("NVIDIA GeForce RTX 4070").click()
+//     await page.locator(".filters-desktop__btn-main.btn-main:visible").click()
+//     //await page.getByRole('button', { name: 'Показать', exact: true }).click()
+//     await page.waitForTimeout(3000);
+//
+//     await page.waitForTimeout(1000);
+// });
